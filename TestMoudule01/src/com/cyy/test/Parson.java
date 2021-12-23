c4520f24eed7b10ce151f9d6fcef3a456a381b01
chenyi <2375795852@qq.com>
2021-12-24 04:34:53 +0800

    在Parson增加了属性

---
+++ TestMoudule01/src/com/cyy/test/Parson.java
@@ -0,0 +1,11 @@
+package com.cyy.test;
+
+/**
+ * @author : chenYi
+ * @date: 2021/12/24 - 12 - 24 - 4:34
+ * @Description: com.cyy.test
+ * @version: 1.0
+ */
+public class Parson {
+    private int age ; //年龄
+}
